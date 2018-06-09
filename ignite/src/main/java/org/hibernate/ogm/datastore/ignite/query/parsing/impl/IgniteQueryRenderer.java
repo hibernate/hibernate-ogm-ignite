1937b6207d8331fc866481b33594582dbbd9c188
Aliaksandr Salauyou (SBT) <ANSolovev.SBT@sberbank.ru>
2018-06-09 16:53:48 +0300

    OGM-1472 after-merge fixes

--- ignite/src/main/java/org/hibernate/ogm/datastore/ignite/query/parsing/impl/IgniteQueryRenderer.java
+++ ignite/src/main/java/org/hibernate/ogm/datastore/ignite/query/parsing/impl/IgniteQueryRenderer.java
@@ -9,19 +9,25 @@ package org.hibernate.ogm.datastore.ignite.query.parsing.impl;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toList;
+import org.hibernate.cfg.NotYetImplementedException;
 import org.hibernate.hql.ast.origin.hql.resolve.path.PropertyPath;
+import org.hibernate.loader.custom.ScalarReturn;
 import org.hibernate.ogm.datastore.ignite.query.impl.IgniteQueryDescriptor;
 import org.hibernate.ogm.datastore.ignite.util.StringHelper;
+import org.hibernate.ogm.model.key.spi.EntityKeyMetadata;
+import org.hibernate.type.Type;
 
 /**
  * @author Victor Kadachigov
  */
 public class IgniteQueryRenderer {
 
-	private static final List<String> ENTITY_COLUMN_NAMES = Collections.unmodifiableList( Arrays.asList( "_KEY", "_VALUE" ) );
+	private static final List<String> ENTITY_COLUMN_NAMES = Collections.unmodifiableList( Arrays.asList( "_KEY", "_VAL" ) );
 
 	// values set by IgniteTreeRenderer
 	String where;
@@ -44,14 +50,69 @@ public class IgniteQueryRenderer {
 	}
 
 
-	private void select(StringBuilder queryBuilder) {
+	/**
+	 * Appends SELECT clause in query builder and returns either
+	 * list of selections if a query is a projection query, or empty
+	 * list if a single entity query
+	 */
+	private List<ScalarReturn> select(StringBuilder queryBuilder) {
 		queryBuilder.append( "SELECT " );
-		String selectionAlias = ( propertyHelper.getSelectionPath() == null )
-			? propertyHelper.findAliasForType( propertyHelper.getRootEntity() )
-			: propertyHelper.getSelectionPath().getFirstNode().getName();
-		queryBuilder
-			.append( selectionAlias ).append( "._KEY, " )
-			.append( selectionAlias ).append( "._VAL " );
+		String rootAlias = propertyHelper.findAliasForType( propertyHelper.getRootEntity() );
+
+		// is selected unqualified root entity (e.g. "from Hypothesis"),
+		// or a single entity defined by alias (e.g. "select h from Hypothesis h")
+		if ( propertyHelper.getSelections().isEmpty()
+			|| ( propertyHelper.getSelections().size() == 1
+			&& propertyHelper.getSelections().get( 0 ).getNodeNamesWithoutAlias().isEmpty() ) ) {
+			String selectionAlias = propertyHelper.getSelections().isEmpty()
+				? rootAlias
+				: propertyHelper.getSelections().get( 0 ).getFirstNode().getName();
+			queryBuilder
+				.append( selectionAlias ).append( "._KEY, " )
+				.append( selectionAlias ).append( "._VAL" );
+			return Collections.emptyList();
+		}
+
+		// else, treat as projection selection
+		List<ScalarReturn> selections = new ArrayList<>();
+		int columnNumber = 0;
+		Iterator<PropertyPath> i = propertyHelper.getSelections().iterator();
+		while ( i.hasNext() ) {
+			PropertyPath path = i.next();
+			String alias = path.getFirstNode().isAlias()
+				? path.getFirstNode().getName() : rootAlias;
+
+			String columnName;
+			List<String> propertyPath = path.getNodeNamesWithoutAlias();
+			String entityType = propertyHelper.getEntityNameByAlias( alias );
+			Type type = propertyHelper.getPropertyType( entityType, propertyPath );
+			if ( type.isEntityType() ) {
+				// though it may be better to load both key and value
+				// in one query, OgmQueryLoader requires only key
+				columnName = "_KEY";
+			}
+			else if ( type.isComponentType() ) {
+				throw new NotYetImplementedException( "Embeddables in projection selection" );
+			}
+			else {
+				columnName = propertyHelper.getColumnName( entityType, propertyPath );
+				EntityKeyMetadata entityKey = propertyHelper.getKeyMetaData( entityType );
+				if ( entityKey.getColumnNames().length == 1
+					&& entityKey.getColumnNames()[0].equals( columnName ) ) {
+					columnName = "_KEY";
+				}
+			}
+			String columnAlias = "col_" + ( columnNumber++ );
+			queryBuilder
+				.append( alias ).append( '.' ).append( columnName )
+				.append( " as " ).append( columnAlias );
+			selections.add( new ScalarReturn( type, columnAlias ) );
+
+			if ( i.hasNext() ) {
+				queryBuilder.append( ',' ).append( ' ' );
+			}
+		}
+		return selections;
 	}
 
 
@@ -65,7 +126,7 @@ public class IgniteQueryRenderer {
 
 	public IgniteQueryParsingResult getResult() {
 		StringBuilder queryBuilder = new StringBuilder();
-		select( queryBuilder );
+		List<ScalarReturn> selections = select( queryBuilder );
 		from( queryBuilder );
 		if ( !StringHelper.isEmpty( from ) ) {
 			queryBuilder.append( ' ' ).append( from );
@@ -77,10 +138,15 @@ public class IgniteQueryRenderer {
 			queryBuilder.append( " ORDER BY " ).append( orderBy );
 		}
 
-		boolean hasScalar = false; // no projections for now
 		IgniteQueryDescriptor queryDescriptor = new IgniteQueryDescriptor(
-			queryBuilder.toString(), indexedParameters, hasScalar );
-		return new IgniteQueryParsingResult( queryDescriptor, ENTITY_COLUMN_NAMES );
+			queryBuilder.toString(), indexedParameters, !selections.isEmpty(),
+			propertyHelper.getKeyMetaData( propertyHelper.getRootEntity() ), selections );
+
+		List<String> selectionAliases = selections.isEmpty()
+			? ENTITY_COLUMN_NAMES
+			: selections.stream().map( ScalarReturn::getColumnAlias ).collect( toList() );
+
+		return new IgniteQueryParsingResult( queryDescriptor, selectionAliases );
 	}
 
 
